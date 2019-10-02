21cfdae657ad0bd23b662c22f15d66f8e109242c
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-02 11:26:27 -0600

    Style menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,7 +5,7 @@ const Menu = ( {items, selectTopic, state} ) => {
   items.pop()
   const nav = items.map((item, i) => {
     return (
-      <h2 id={item} key={i} onClick={() => selectTopic(state[item])}>{item}</h2>
+        <h2 id={item} key={i} onClick={() => selectTopic(state[item])}>{item}</h2>
     )
   })
 
