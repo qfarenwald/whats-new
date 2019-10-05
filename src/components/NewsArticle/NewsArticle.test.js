b7bc3b06fceadf5f1a8aba865fab8d5e330f5e4b
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-05 11:31:35 -0600

    Add snaphot test set up and snapshot of newsarticle

---
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewsArticle from './NewsArticle';
+
+describe('NewsArticle', () => {
+  it('should match the snapshot with all data pass in correctly', () => {
+    const wrapper = shallow(
+      <NewsArticle key={1}
+        img='https://theknow.denverpost.com/wp-content/uploads/2016/03/CDXXTHEWHODSC_4082x.jpg'
+        headline='The Who postpones Denver Concert at the Pepsi Center'
+        description='Classic-rock band The Who has postponed its Denver concert, set for Sunday, Sept. 29, after an illness robbed singer Roger Daltrey of his voice, the band said on Twitter.'
+        url='https://theknow.denverpost.com/2019/09/27/the-who-postpones-denver-concert/225182/'
+      />
+    )
+    expect(wrapper).toMatchSnapshot();
+  })
+})
