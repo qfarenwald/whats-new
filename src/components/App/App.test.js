cf65861b04ea6244be8e79fb53eeeef368a5ef8a
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-05 12:19:28 -0600

    Add snapshot for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import SearchForm from '../SearchForm/SearchForm';
+import Menu from '../Menu/Menu';
+import NewsContainer from '../NewsContainer/NewsContainer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+
+  it('should match snapshot with correct data', () => {
+
+    const wrapper = shallow(
+      <App />
+    )
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
