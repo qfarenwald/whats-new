8edbd4cf0ab243d5bf5cf6704fcae546128b52bc
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-06 19:03:24 -0600

    Updates

---
+++ precache-manifest.5801172598eb36a461ba155dbfc219dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9b3c91612aee0fd0f35e54928e35ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a76d16cbe20f147d4fa",
+    "url": "./static/css/main.1f1c44e5.chunk.css"
+  },
+  {
+    "revision": "34c8844994998be97242",
+    "url": "./static/js/2.05295a0a.chunk.js"
+  },
+  {
+    "revision": "9a76d16cbe20f147d4fa",
+    "url": "./static/js/main.330cd3a8.chunk.js"
+  },
+  {
+    "revision": "a7ba3a89543cc5257511",
+    "url": "./static/js/runtime-main.bf980d17.js"
+  }
+]);
